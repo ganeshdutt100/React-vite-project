d3394f5000a7578a5b72fa4a792d4a81dec00f1b
Ganesh Dutt <gdutt2304@gmail.com>
2024-04-30 13:37:14 +0530

    class 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { Navbar } from './components/Navbar';
+import { Sec1 } from './components/Sec1';
+
+function App() {
+  
+
+  return (
+    <>
+  <Navbar/>
+   <Sec1/>
+   <Sec1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sec1.jsx
@@ -0,0 +1,20 @@
+
+
+export const Sec1 = () => {
+  return (
+    <div>
+        <div className="container mt-5">
+            <div className="row row-cols-1 row-cols-lg-2">
+                <div className="col">
+                    <h1>Lorem ipsum dolor sit amet.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore aut deserunt ea nihil eaque at accusamus sed consequuntur voluptates nobis quae, animi temporibus itaque ut officiis inventore. Praesentium, tenetur facilis.</p>
+                    <button className="btn btn-danger"> click me</button>
+                </div>
+                <div className="col">
+                    <img src="https://img.freepik.com/free-photo/rear-view-programmer-working-all-night-long_1098-18697.jpg?t=st=1714462464~exp=1714466064~hmac=60354ab328491a48480d5cca203ac82f20764008f75da37dde8a0d78c3e78806&w=900" alt="" className=" img-fluid"/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
